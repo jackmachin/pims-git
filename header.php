ab0259e6c45b2e9b25cb3156fda3f7b732af7912
Jack Machin <jack.machin125@gmail.com>
2016-10-06 09:54:35 +0100

    Update header.php
    Removing GA tracker as now dealt with in plugin.

--- header.php
+++ header.php
@@ -65,24 +65,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
 
 <?php flush(); ?>
-<!-- GA -->
-<script type="text/javascript">
-
-            var allowed = $.cookie("pimsallowed");
-            if (allowed == null) {
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-20034801-9']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-            }
-<!-- END GA -->
-
-</script>
 
 </head>
 
